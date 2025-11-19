dc4842dedc274adafab3b58836d68ec6c74fe1db
phoenix-of-dawn <eliobenoit@live.com>
2025-11-19 10:52:24 -0500

    Refactor images

---
+++ Smash-And-Splatter/src/smashandsplatter/controller/splatter/SplatterSidebarController.java
@@ -0,0 +1,16 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class SplatterSidebarController {
+
+    @FXML
+    private Button Submit;
+
+    @FXML
+    private TextField xVelocity;
+
+    @FXML
+    private TextField yPos;
+
+}
