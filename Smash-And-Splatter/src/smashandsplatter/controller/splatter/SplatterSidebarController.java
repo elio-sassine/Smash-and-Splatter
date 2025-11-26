ee043369b9078dd4e0d340bdc379a0b8047c7dbf
phoenix-of-dawn <eliobenoit@live.com>
2025-11-25 20:20:21 -0500

    add fields

--- Smash-And-Splatter/src/smashandsplatter/controller/splatter/SplatterSidebarController.java
+++ Smash-And-Splatter/src/smashandsplatter/controller/splatter/SplatterSidebarController.java
@@ -1,11 +1,24 @@
+package smashandsplatter.controller.splatter;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 public class SplatterSidebarController {
-
     @FXML
     private Button Submit;
+
+    @FXML
+    private TextField finalYVelocity;
+
+    @FXML
+    private TextField initialYVelocity;
+
+    @FXML
+    private TextField time;
+
+    @FXML
+    private TextField xDistance;
 
     @FXML
     private TextField xVelocity;
