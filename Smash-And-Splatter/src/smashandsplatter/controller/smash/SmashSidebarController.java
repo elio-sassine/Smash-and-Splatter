b82ffb2771caa2505590bce1e6b85f0c11a6630c
phoenix-of-dawn <eliobenoit@live.com>
2025-11-26 10:26:30 -0500

    Add SmashSidebar Controller and View

---
+++ Smash-And-Splatter/src/smashandsplatter/controller/smash/SmashSidebarController.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package smashandsplatter.controller.smash;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author eliob
+ */
+public class SmashSidebarController implements Initializable {
+
+    @FXML
+    private TextField answerTorque;
+
+    @FXML
+    private Button submit;
+
+    @FXML
+    private TextField torque1;
+
+    @FXML
+    private TextField torque2;
+
+    
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
